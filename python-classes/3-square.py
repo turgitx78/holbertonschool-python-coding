db1a4358dcb3e8ad7cff16b8048fa303a4fcdb7a
turgitx78 <turgutazizli3@gmail.com>
2025-12-23 04:06:26 +0000

    Add size property with getter/setter to Square

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size: size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
